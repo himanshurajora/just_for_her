3b840300a8288cd01a376cc5c2cba17847bc7696
Himanshu Jangid <iamahalofan@gmail.com>
2021-12-07 22:09:11 +0530

    fonts and components

--- App.tsx
+++ App.tsx
@@ -8,13 +8,17 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -27,89 +31,93 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+
+const HeadingText = (props: { text: string }) => {
+  let colorScheme = useColorScheme();
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Text style={
+      [
+        {
+          color: '#333333',
+          fontSize: 30,
+          alignSelf: 'center',
+          marginBottom: 20,
+          fontFamily: 'Beauty',
+        },
+      ]
+    }>
+      {props.text}
+    </Text>
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const GetInButton = (props: { onPress: () => void }) => {
+  
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <TouchableOpacity
+      style={[
+        {
+          backgroundColor: '#FF559D',
+          padding: 10,
+          borderRadius: 15,
+          margin: 20,
+          width: '50%',
+        }
+      ]}
+      onPress={props.onPress}
+    >
+      <Text style={{ color: 'white', fontSize: 18, alignSelf:"center", fontWeight: '900' }}>
+        Get In
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+
+
+const App = () => {
+
+  const colorScheme = useColorScheme();
+  const [count, setcount] = useState(0);
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <HeadingText text={'Just For You 💖'} />
+        <TextInput style={styles.nameInput} placeholder='Enter Your Name'>
+        
+        </TextInput>
+        <GetInButton onPress={()=>{}}></GetInButton>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container : {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  nameInput: {
+    width: '80%',
+    height: 40,
+    borderColor: '#FFB3D1',
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    borderRadius: 15,
+    alignSelf: 'center',
+    textAlign: 'center',
+    minHeight: 50,
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: '#fff'
+    // fontFamily: 'Beauty',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
